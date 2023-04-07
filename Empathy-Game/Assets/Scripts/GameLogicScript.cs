3594656b8d496d366690886088b1a8bb7608a849
Maya Faerman <mayafa@mta.ac.il>
2023-04-07 16:25:18 +0300

    draw card logic script and game objects slots

--- Empathy-Game/Assets/Scripts/GameLogicScript.cs
+++ Empathy-Game/Assets/Scripts/GameLogicScript.cs
@@ -2,10 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLogicScript : MonoBehaviour
 {
     public TimerScript roundTimer;
+
+    public List<CardScript> deck = new List<CardScript>();
+    public Transform[] Slots; // the cads slots objects
+    public bool[] availableSlot;
+
+    public void DrawCard()
+    {
+        if (deck.Count >= 1)
+        {
+            CardScript card = deck[Random.Range(0, deck.Count)];
+
+            for(int i = 0; i < availableSlot.Length; i++)
+            {
+                if(availableSlot[i] == true)
+                {
+                    card.gameObject.SetActive(true);
+                    card.transform.position = Slots[i].position;
+                    availableSlot[i] = false;
+                    deck.Remove(card);
+                    return;
+                }
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
