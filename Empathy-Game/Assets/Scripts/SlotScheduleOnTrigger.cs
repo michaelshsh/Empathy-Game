bff5925e0d019a5ef8ccb3c72bebdb6daac5a3bd
Tomer <tomerne@mta.ac.il>
2023-05-05 19:16:56 +0300

    test

--- Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
+++ Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
@@ -7,12 +7,32 @@ public class SlotScheduleOnTrigger : MonoBehaviour
 {
     public GameObject slot;
     public TextMeshPro UIText;
+    private CardScript card = null;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.SetActive(false);
-        collision.gameObject.transform.localPosition = Vector3.zero;
-        GameObject parent = collision.gameObject.transform.Find("Text").gameObject;
-        GameObject txt = parent.transform.Find("FreeText_Var").gameObject;
-        UIText.text = txt.GetComponent<TextMeshPro>().text;
+        Debug.Log("entering OnTriggerEnter2D");
+        if (card == null)
+        {
+            collision.gameObject.SetActive(false);
+            GameObject parent = collision.gameObject.transform.Find("Text").gameObject;
+            GameObject txt = parent.transform.Find("FreeText_Var").gameObject;
+            UIText.text = txt.GetComponent<TextMeshPro>().text;
+            card = collision.gameObject.GetComponent<CardScript>();
+            slot.gameObject.GetComponent<Collider2D>().isTrigger = false;
+            Debug.Log("card was set to be the new card");
+
+        }  
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        if (card != null)
+        {
+            card.gameObject.transform.position = Vector3.zero;
+            card.gameObject.SetActive(true);             
+            card = null;
+            Debug.Log("card set to null");
+            //slot.gameObject.GetComponent<Collider2D>().isTrigger = true;
+        }
     }
 }
