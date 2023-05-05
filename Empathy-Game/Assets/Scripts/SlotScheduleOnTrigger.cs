f0c1813d29db2fc21125218f89adac07fc213ed4
Tomer <tomerne@mta.ac.il>
2023-05-05 23:58:05 +0300

    Update SlotScheduleOnTrigger.cs
    fixing code to be more readable

--- Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
+++ Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
@@ -5,42 +5,38 @@ using UnityEngine;
 
 public class SlotScheduleOnTrigger : MonoBehaviour
 {
-    public GameObject slot;
-    public TextMeshPro UIText;
+    [SerializeField]
+    private GameObject slot;
+    [SerializeField]
+    private TextMeshPro UIText;
     private CardScript card = null;
     private bool mouseDown = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("entering OnTriggerEnter2D");
         if (card == null && !mouseDown)
         {
             collision.gameObject.SetActive(false);
-            GameObject parent = collision.gameObject.transform.Find("Text").gameObject;
-            GameObject txt = parent.transform.Find("FreeText_Var").gameObject;
+            GameObject parent = collision.gameObject.transform.Find("Text").gameObject; // get parent of FreeText_Var
+            GameObject txt = parent.transform.Find("FreeText_Var").gameObject; // get FreeText_Var
             UIText.text = txt.GetComponent<TextMeshPro>().text;
             card = collision.gameObject.GetComponent<CardScript>();
-            Debug.Log("card was set to be the new card");
-
         }
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Clicked");
         mouseDown = true;
         if (card != null)
         {
             card.gameObject.transform.position = new Vector3(50, -50, 0);
-            UIText.text = "";
+            UIText.text = string.Empty;
             card.gameObject.SetActive(true);
             card = null;
-
         }
     }
 
     private void OnMouseUp()
     {
-        Debug.Log("Mouse Up");
         mouseDown = false;
     }
 }
