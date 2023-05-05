1d7b61bb43b8bd4be0818f46fc5a342e18426683
Tomer <tomerne@mta.ac.il>
2023-05-05 23:46:30 +0300

    adding the option to remove card from schedule slot

--- Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
+++ Empathy-Game/Assets/Scripts/SlotScheduleOnTrigger.cs
@@ -8,31 +8,39 @@ public class SlotScheduleOnTrigger : MonoBehaviour
     public GameObject slot;
     public TextMeshPro UIText;
     private CardScript card = null;
+    private bool mouseDown = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("entering OnTriggerEnter2D");
-        if (card == null)
+        if (card == null && !mouseDown)
         {
             collision.gameObject.SetActive(false);
             GameObject parent = collision.gameObject.transform.Find("Text").gameObject;
             GameObject txt = parent.transform.Find("FreeText_Var").gameObject;
             UIText.text = txt.GetComponent<TextMeshPro>().text;
             card = collision.gameObject.GetComponent<CardScript>();
-            slot.gameObject.GetComponent<Collider2D>().isTrigger = false;
             Debug.Log("card was set to be the new card");
 
-        }  
+        }
     }
+
     private void OnMouseDown()
     {
         Debug.Log("Clicked");
+        mouseDown = true;
         if (card != null)
         {
-            card.gameObject.transform.position = Vector3.zero;
-            card.gameObject.SetActive(true);             
+            card.gameObject.transform.position = new Vector3(50, -50, 0);
+            UIText.text = "";
+            card.gameObject.SetActive(true);
             card = null;
-            Debug.Log("card set to null");
-            //slot.gameObject.GetComponent<Collider2D>().isTrigger = true;
+
         }
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Mouse Up");
+        mouseDown = false;
     }
 }
