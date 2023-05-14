5a2365e3210c40d13dfdd15f3e5a67c2584dc7da
Maya Faerman <mayafa@mta.ac.il>
2023-05-14 19:45:04 +0300

    Logic

--- Empathy-Game/Assets/Scripts/SummeryScript.cs
+++ Empathy-Game/Assets/Scripts/SummeryScript.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class SummeryScript : MonoBehaviour
 {
     public static SummeryScript InstanceSummeryManager { get; private set; }
-    
+    [field: SerializeField]
+    private List<int> PersonalPointsPerRound;
+    [field: SerializeField]
+    private List<int> GroupPointsPerRound;
+    [field: SerializeField]
+    private int round = 0;
+
     private void Awake()
     {
         if (InstanceSummeryManager != null && InstanceSummeryManager != this)
@@ -17,6 +23,25 @@ public class SummeryScript : MonoBehaviour
             InstanceSummeryManager = this;
         }
     }
+
+    private void UpdatePersonalPoints(int point)
+    {
+        PersonalPointsPerRound[round - 1] = PersonalPointsPerRound[round - 1] + point;
+    }
+
+    private void UpdateGroupPoints(int point)
+    {
+        GroupPointsPerRound[round - 1] = GroupPointsPerRound[round - 1] + point;
+    }
+
+    private void StartRound()
+    {
+        round++;
+        PersonalPointsPerRound.Add(0);
+        GroupPointsPerRound.Add(0);
+    }
+    //comparing this round with privious
+
     // Start is called before the first frame update
     void Start()
     {
