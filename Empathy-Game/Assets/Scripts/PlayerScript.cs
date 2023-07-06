63e867e29d7b882a4a602bc7a19f0a166c2d3a7b
Michael Sh Sh <96913637+michaelshsh@users.noreply.github.com>
2023-07-06 20:13:07 +0300

    adding name to player scrpit

--- Empathy-Game/Assets/Scripts/PlayerScript.cs
+++ Empathy-Game/Assets/Scripts/PlayerScript.cs
@@ -11,6 +11,7 @@ public class PlayerScript : MonoBehaviour
     private PlayerLabels.LabelEnum mylabel;
     [field: SerializeField]
     public StatsScriptableObject Score;
+    [SerializeField] public string PlayerName { get; private set; }
 
     // Start is called before the first frame update
     void Start()
