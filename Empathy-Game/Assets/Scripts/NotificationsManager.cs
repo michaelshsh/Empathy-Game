c72dccb8331039015a30c803de01ee6ca2cc027e
Maya Faerman <mayafa@mta.ac.il>
2023-09-05 18:04:33 +0300

    adding sending notifications to all clients by label.

--- Empathy-Game/Assets/Scripts/NotificationsManager.cs
+++ Empathy-Game/Assets/Scripts/NotificationsManager.cs
@@ -16,18 +16,13 @@ public class NotificationsManager : NetworkBehaviour
         NotificationsManager.Singleton = this;
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
-
     public void SendNotification(string msg, string _from = null, string _to = null)
     {
         if (string.IsNullOrWhiteSpace(msg)) return;
 
         string N = _from + msg;
-        SendNotificationServerRpc(N);
+       
+        SendNotificationServerRpc(N, _to);
     }
 
     void AddNotification(string msg)
@@ -37,14 +32,25 @@ public class NotificationsManager : NetworkBehaviour
     }
 
     [ServerRpc(RequireOwnership = false)]
-    void SendNotificationServerRpc(string msg)
+    void SendNotificationServerRpc(string msg, string _to)
     {
-        ReceiveNotificationClientRpc(msg);
+        var Players = FindObjectsOfType<PlayerScript>();
+        List<ulong> ids = new List<ulong>();
+
+        foreach (var player in Players)
+        {
+            if (_to.Equals(player.mylabel.Name))
+            {
+                ids.Add(player.OwnerClientId);
+            }
+        }
+        ReceiveNotificationClientRpc(msg, new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong>(ids) } });//sending to the server list of clients IDs that need to get notification.
     }
 
     [ClientRpc]
-    void ReceiveNotificationClientRpc(string msg)
+    void ReceiveNotificationClientRpc(string msg, ClientRpcParams clientRpcParams = default)//only the clients that the id in the params
     {
         NotificationsManager.Singleton.AddNotification(msg);
     }
+
 }
