23845863ffbfa8f598205d5b122f966d1f156e32
Maya Faerman <mayafa@mta.ac.il>
2023-09-07 15:41:08 +0300

    notifications tested and added remove Notification

--- Empathy-Game/Assets/Scripts/NotificationsManager.cs
+++ Empathy-Game/Assets/Scripts/NotificationsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using TMPro;
+using static Constants.PlayerLabels;
 
 public class NotificationsManager : NetworkBehaviour
 {
@@ -16,17 +17,18 @@ public class NotificationsManager : NetworkBehaviour
         NotificationsManager.Singleton = this;
     }
 
-    public void SendNotification(string msg, string _from = null, string _to = null)
+    public void SendNotification(string msg, string _from, string _to)
     {
+        Debug.Log("entered send notification");
         if (string.IsNullOrWhiteSpace(msg)) return;
 
-        string N = _from + msg;
-       
+        string N = _from + " " +  msg;
         SendNotificationServerRpc(N, _to);
     }
 
     void AddNotification(string msg)
     {
+        Debug.Log("added notification");
         Notification N = Instantiate(notification, NotificationWindow.transform);
         N.SetText(msg);
         PopUpWindow.Singleton.Addqueue("You received an invitation to schedule an appointment");
@@ -35,13 +37,16 @@ public class NotificationsManager : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     void SendNotificationServerRpc(string msg, string _to)
     {
+        Debug.Log("in server");
         var Players = FindObjectsOfType<PlayerScript>();
         List<ulong> ids = new List<ulong>();
 
         foreach (var player in Players)
         {
-            if (_to.Equals(player.mylabel.Name))
+            Debug.Log(Constants.PlayerLabels.EnumToString(player.mylabel.Value));
+            if (_to.Equals(Constants.PlayerLabels.EnumToString(player.mylabel.Value)))
             {
+                Debug.Log("added " + player.OwnerClientId + "to the list");
                 ids.Add(player.OwnerClientId);
             }
         }
