f518a6f498b0b67147832b087869fffa893941aa
Maya Faerman <mayafa@mta.ac.il>
2023-09-08 23:20:04 +0300

    adding summery window + animation

---
+++ Empathy-Game/Assets/Scripts/SummeryAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummeryAnimation : MonoBehaviour
+{
+    public Transform box;
+    public CanvasGroup background;
+    public GameObject summeryGroup;
+
+    public void OnOpeningWindow()
+    {
+        background.alpha = 0;
+        background.LeanAlpha(1, 0.5f);
+
+        box.localPosition = new Vector2(0, -Screen.height);
+        box.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+    }
+
+    public void OnClosingWindow()
+    {
+        background.LeanAlpha(0, 0.5f);
+        box.LeanMoveLocalY(-Screen.height, 0.5f).setEaseInExpo().setOnComplete(OnComplete);
+    }
+
+    void OnComplete()
+    {
+        summeryGroup.SetActive(false);
+    }
+}
