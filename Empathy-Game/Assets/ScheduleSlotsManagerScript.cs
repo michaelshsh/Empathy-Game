f9e1fb91bfba7860010bec598472f7d3a0d3b97e
Tomer <tomerne@mta.ac.il>
2023-09-11 23:26:09 +0300

    fixing occupied slots after round ends

--- Empathy-Game/Assets/ScheduleSlotsManagerScript.cs
+++ Empathy-Game/Assets/ScheduleSlotsManagerScript.cs
@@ -162,6 +162,7 @@ public class ScheduleSlotsManagerScript : MonoBehaviour
         {
             slot.UIText.text = string.Empty;
             slot.TaskCard = null;
+            slot.isUsedAsCoopCard = false;
         }
     }
 
